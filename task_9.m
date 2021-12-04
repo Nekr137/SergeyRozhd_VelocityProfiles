29c2c4d180ffafd3fe1b96a7fa27e0ee8e880f73
Nekr137 <denekra@yandex.ru>
2021-12-04 10:31:10 +0700

    Task-9. Adjusted time axis as common for all four contours.

--- task_9.m
+++ task_9.m
@@ -1,27 +1,32 @@
 function task_9
 
-% U = load('task_9_U(ABS1).txt');
-% V = load('task_9_V(ABS1).txt');
-
 [Vn.ABS1, Ve.ABS1, T.ABS1, H0.ABS1] = sr_load_abs_data('ABS1_adcp300_dir.txt', 'ABS1_adcp300_mag.txt');
 [Vn.ABS2, Ve.ABS2, T.ABS2, H0.ABS2] = sr_load_abs_data('ABS2_dvs1_dir3.txt', 'ABS2_dvs1_mag3.txt');
 
-step.ABS1 = 50.0;
+step.ABS1 = 40.0;
 step.ABS2 = 10.0;
 
-buildContourPlot(subplot(512), T.ABS1, Vn.ABS1, H0.ABS1, step.ABS1, 'ABS1, U-component');
-buildContourPlot(subplot(513), T.ABS1, Ve.ABS1, H0.ABS1, step.ABS1, 'ABS1, V-component');
-buildContourPlot(subplot(514), T.ABS2, Vn.ABS2, H0.ABS2, step.ABS2, 'ABS2, U-component');
-buildContourPlot(subplot(515), T.ABS2, Ve.ABS2, H0.ABS2, step.ABS2, 'ABS2, V-component');
+timeLim = [
+    min([T.ABS1(1) T.ABS2(1)])
+    max([T.ABS1(end) T.ABS2(end)])
+];
 
+buildContourPlot(subplot(512), T.ABS1, timeLim, Vn.ABS1, H0.ABS1, step.ABS1, 'ABS1, U-component');
+buildContourPlot(subplot(513), T.ABS1, timeLim, Ve.ABS1, H0.ABS1, step.ABS1, 'ABS1, V-component');
+buildContourPlot(subplot(514), T.ABS2, timeLim, Vn.ABS2, H0.ABS2, step.ABS2, 'ABS2, U-component');
+buildContourPlot(subplot(515), T.ABS2, timeLim, Ve.ABS2, H0.ABS2, step.ABS2, 'ABS2, V-component');
 
+colormap jet
 
 end
 
-function buildContourPlot(ax, T, V, H, levelStep, titl)
+function buildContourPlot(ax, T, timeLim, V, H, levelStep, titl)
 % @param [in] ax - axes
 % @param [in] T  - time vector
+% @param [in] timeLim - [tLim0 tLim1]
 % @param [in] V  - velocity component
+% @param [in] H  - depth
+% @param [in] levelStep - how frequencly lines will appeared
 % @param [in] titl - title
 
 [M,c] = contourf(ax,T,H,V);
@@ -29,9 +34,12 @@ function buildContourPlot(ax, T, V, H, levelStep, titl)
 % c.LineWidth = 0.01;
 set(c,'EdgeColor',[0.5 0.5 0.5]);
 set(c,'LevelStep',levelStep);
-title(titl);
+title(titl,'FontSize',10,'FontWeight','normal');
 ylabel('Depth, m');
+% colorbar;
+caxis([-200 200]);
 
-
+set(ax,'XLim', timeLim);
+datetick('x', 'mm/dd HH:MM', 'keeplimits', 'keepticks');
 
 end
