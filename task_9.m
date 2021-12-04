e791b3dedbdcc34be693b5da5d6577415096e556
Nekr137 <denekra@yandex.ru>
2021-12-04 11:47:21 +0700

    Task-9. Finished time interval subplot.

--- task_9.m
+++ task_9.m
@@ -7,8 +7,8 @@ step.ABS1 = 40.0;
 step.ABS2 = 10.0;
 
 timeLim = [
-    min([T.ABS1(1) T.ABS2(1)])
-    max([T.ABS1(end) T.ABS2(end)])
+    max([T.ABS1(1) T.ABS2(1)])
+    min([T.ABS1(end) T.ABS2(end)])
 ];
 
 buildContourPlot(subplot(512), T.ABS1, timeLim, Vn.ABS1, H0.ABS1, step.ABS1, 'ABS1, U-component');
@@ -17,6 +17,42 @@ buildContourPlot(subplot(514), T.ABS2, timeLim, Vn.ABS2, H0.ABS2, step.ABS2, 'AB
 buildContourPlot(subplot(515), T.ABS2, timeLim, Ve.ABS2, H0.ABS2, step.ABS2, 'ABS2, V-component');
 
 colormap jet
+
+%% Time interval
+
+% Loading the data
+[Vn.wind, Ve.wind, T.wind, ~] = sr_load_task8('task_8_veter(copy).dat');
+
+% Removing broken data - when time is zero
+remIndices = find(T.wind < 1);
+Vn.wind(remIndices) = [];
+Ve.wind(remIndices) = [];
+T.wind(remIndices) = [];
+H0.wind = [1];
+
+% Extracting the interesting time interval
+% windTimeInterval = [datenum('2021-08-09, 20:41', 'yyyy-mm-dd, HH:MM') T.wind(end)];
+% windTimeInterval = [datenum('2021-08-09, 20:32', 'yyyy-mm-dd, HH:MM') T.wind(end)];
+[Vn.windInterv, Ve.windInterv, T.windInterv] = sr_extract_time_interval(Vn.wind, Ve.wind, T.wind, timeLim);
+
+% Find mean values
+w = 60 * 40;
+Vn.wndm = sr_find_average_data(Vn.windInterv, w); 
+Ve.wndm = sr_find_average_data(Ve.windInterv, w); 
+T.wndm  = sr_find_average_data(T.windInterv, w); 
+
+ax = subplot(511);
+hold on; box on; grid on;
+set(ax,'YLim', [0 2.0]);
+set(ax,'XLim', timeLim);
+set(ax, 'YDir', 'reverse'); % Made y axis reversed
+
+% screenCoef = 1 / length(H0) / 10;
+screenCoef = 1.0 / 30.0;
+xyScreenRatio = 10.0;
+sr_visualize_profiles(ax, Vn.wndm, Ve.wndm, T.wndm, H0.wind, [1 0 0], screenCoef, xyScreenRatio);
+sr_put_scale_vector(ax, screenCoef, xyScreenRatio);
+datetick('x', 'mm/dd HH:MM', 'keeplimits', 'keepticks');
 
 end
 
@@ -38,8 +74,6 @@ title(titl,'FontSize',10,'FontWeight','normal');
 ylabel('Depth, m');
 % colorbar;
 caxis([-200 200]);
-
 set(ax,'XLim', timeLim);
 datetick('x', 'mm/dd HH:MM', 'keeplimits', 'keepticks');
-
 end
