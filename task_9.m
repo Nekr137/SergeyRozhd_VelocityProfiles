5e2a3ac9a2e363d365b333044dde01b3ce5b4e17
ndenis <denekra@yandex.ru>
2021-12-05 07:06:34 +0700

    Task-9. Added text labels to the contours.

--- task_9.m
+++ task_9.m
@@ -3,8 +3,8 @@ function task_9
 [Vn.ABS1, Ve.ABS1, T.ABS1, H0.ABS1] = sr_load_abs_data('ABS1_adcp300_dir.txt', 'ABS1_adcp300_mag.txt');
 [Vn.ABS2, Ve.ABS2, T.ABS2, H0.ABS2] = sr_load_abs_data('ABS2_dvs1_dir3.txt', 'ABS2_dvs1_mag3.txt');
 
-step.ABS1 = 40.0;
-step.ABS2 = 10.0;
+step.ABS1 = 80.0;
+step.ABS2 = 20.0;
 
 timeLim = [
     max([T.ABS1(1) T.ABS2(1)])
@@ -66,7 +66,7 @@ function buildContourPlot(ax, T, timeLim, V, H, levelStep, titl)
 % @param [in] titl - title
 
 [M,c] = contourf(ax,T,H,V);
-% 'ShowText','on'
+set(c,'ShowText','on');
 % c.LineWidth = 0.01;
 set(c,'EdgeColor',[0.5 0.5 0.5]);
 set(c,'LevelStep',levelStep);
