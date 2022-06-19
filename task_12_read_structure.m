e86b6a19f123369793b488d2fb0d131ed0a38544
ndenis <denekra@yandex.ru>
2022-06-20 01:13:46 +0700

    Task-13. Russian language comments on task_12_read_structure.m file

--- task_12_read_structure.m
+++ task_12_read_structure.m
@@ -1,88 +1,90 @@
 function task_12_read_structure()
-% Scan all files in the `myDir` directory 
-% and perform conversion
+% Программа сканирует все файлы в `myDir` директории.
 
-% Directory name
-%myDir = 'task12_data';
-myDir = 'task13_data';
+myDir = 'task13_data'; % указываем имя директории, где ищем файлы для конвертации
+myFiles = dir(fullfile(myDir,'*.000')); % извлекаем все пути до файлов, оканчивающихся на .000
 
-% All files to convert
-myFiles = dir(fullfile(myDir,'*.000'));
+for k = 1:length(myFiles) % цикл по всем файлам, `k` - индекс цикла
+   baseFileName = myFiles(k).name; % берём имя `k`-того имя файла
+   fullFileName = fullfile(myDir, baseFileName); % полный путь до этого файла
+   fprintf(1, 'Now converting %s\n', fullFileName); % вывод в консоль матлаба
 
-% For each file
-for k = 1:length(myFiles)
-   baseFileName = myFiles(k).name;
-   fullFileName = fullfile(myDir, baseFileName);
-   fprintf(1, 'Now converting %s\n', fullFileName);
-   
-   % convertFile(fullFileName);
-   convertFile2(fullFileName); % run conversion
+
+   %% конвертация с помощью rdradcp (расскомментировать, чтобы работало)
+   % convertFile(fullFileName); 
+
+   %% конвертация с помощью rdpadcp
+   convertFile2(fullFileName);
 end
 end
 
-% Copied some staff from the
-% rdi_read_moored_plotout_ADCP_BLackSea_Feodosiya_03092019.m
 function convertFile2(fname)
+% Функция, конвертирующая данные с помощью подпрограммы rdpadcp
+% Часть кода взята из программы
+% rdi_read_moored_plotout_ADCP_BLackSea_Feodosiya_03092019.m
 
-% Print something if filed doesn't exist
-if (~exist(fname, 'file'))
-    disp('Failed to find a file');
+if (~exist(fname, 'file')) % Если вдруг файл `fname` не существует
+    disp('Failed to find a file'); % выводим это в консоль матлаба
 end
 
-% The try block. 
-% If something happened in this block the `catch` 
-% section will be evaluated
-try
-    % It takes forewere to read file via ADCP.
-    % Therefore we decided to store (cache) read result.
-    cachedFilename = [fname '_cached_task13.mat'];
+
+% try-блок. Если что-то случится в этом блоке (например файл не существует или повреждён)
+% то будет выполнен catch-блок
+try 
+    % Здесь мы пытаемся загрузить данные. 
+    % Файл данных тяжелый, поэтому, один раз загрузив,
+    % мы можем его сохранить в виде *.mat файла.
+    % И при следущей попытки загрузить этот же файл мы
+    % просто загружаем этот *.mat файл, который загрзуится быстро.
+    % Это что-то типа кэша.
+
+    cachedFilename = [fname '_cached_task13.mat']; % придумаем имя кэш-файла
     
-    if isfile(cachedFilename) % if cache file exist -> read cache
-        load(cachedFilename, 'ADCP');
-    else                      % else read file via rdpadcp
-        ADCP=rdpadcp(fname,1,'ref','bottom','des','yes');
-        save(cachedFilename, 'ADCP'); % cache file
+    if isfile(cachedFilename) % если кэш файл уже имеется, 
+        load(cachedFilename, 'ADCP'); % то загружаем кэш
+    else                                                  % иначе
+        ADCP=rdpadcp(fname,1,'ref','bottom','des','yes'); % грзуим сам файл. 
+        save(cachedFilename, 'ADCP');                     % И сохраняем (кэшируем)
     end
-catch
-    disp(['failed to proc ' fname ' using the rdpadcp']);
+catch % если что-то случилось
+    disp(['failed to proc ' fname ' using the rdpadcp']); % то выводим в консоль
 end
 
-% Getting values from ADCP structure.
-% The code below was copied from the
+% Извлекаем данные из структуры ADCP
+% Код ниже скопирован из функции
 % `rdi_read_moored_plotout_ADCP_BLackSea_Feodosiya_03092019.m`
-Vn=ADCP.north_vel;
-Ve=ADCP.east_vel;
-Vz=ADCP.vert_vel;
-t=ADCP.mtime;
+
+Vn=ADCP.north_vel; % Извлекаем из структуры north_vel
+Ve=ADCP.east_vel;% Извлекаем из структуры east_vel
+Vz=ADCP.vert_vel;% Извлекаем из структуры vert_vel
+t=ADCP.mtime; % извлекаем из структуры mtime
 L0=60;
-H0=ADCP.config.range+ADCP.config.adcp_depth;
-Vn(L0+1:end,:)=[];Ve(L0+1:end,:)=[];Vz(L0+1:end,:)=[];
-H0(L0+1:end)=[];
-V=sqrt(Vn.*Vn+Ve.*Ve); % the amplitude of the velocity
-Ves=smoothdata(Ve,2,'movmean',73,'omitnan'); % smoothing
-Vns=smoothdata(Vn,2,'movmean',73,'omitnan'); % smooxthing
-Vs=sqrt(Vns.*Vns+Ves.*Ves);
+H0=ADCP.config.range+ADCP.config.adcp_depth; % глубина (равна `range` + `adcp_depth` из настроек)
+Vn(L0+1:end,:)=[]; % обрезаем последние глубины, оставляем только `L0` первых штук
+Ve(L0+1:end,:)=[]; % обрезаем последние глубины, оставляем только `L0` первых штук
+Vz(L0+1:end,:)=[]; % обрезаем последние глубины, оставляем только `L0` первых штук
+H0(L0+1:end)=[]; % обрезаем последние глубины, оставляем только `L0` первых штук
+V=sqrt(Vn.*Vn+Ve.*Ve); % амплитуда скорости (здесь не используется сейчас)
+Ves=smoothdata(Ve,2,'movmean',73,'omitnan'); % сглаживание Ve скользящее среднее, игнорируем NaN
+Vns=smoothdata(Vn,2,'movmean',73,'omitnan'); % cглаживание Vn скользящее среднее, игнорируем NaN
+Vs=sqrt(Vns.*Vns+Ves.*Ves); % амплитуда скорости (здесь не используется сейчас)
 
-% Create new file for writing the data
-f = fopen([fname '_output_task_13.txt'],'w');
-% Writing the head
-fprintf(f, "%-10s%-10s%-24s%-20s%-20s\n", "station", "depth", "date", "Vn", "Ve");
+f = fopen([fname '_output_task_13.txt'],'w'); % открываем файл на запись
+fprintf(f, "%-10s%-10s%-24s%-20s%-20s\n", "station", "depth", "date", "Vn", "Ve"); % пишем столбцы-шапку
 
-for k1 = 1:length(H0) % for each station
-    depth = H0(k1);
-    dateStr = datestr(t(k1)); % convertint the utc format date to date string
-    aEastVel = Ves(k1,:);     % vector of `e` velocities for this depth
-    aNorthVel = Vns(k1,:);    % vector of `n` velocities for this depth
-    for k2 = 1:length(aEastVel)
-      eastVel = aEastVel(k2);
-      northVel = aNorthVel(k2);
-      number = 1;
-      % Writing the row of data
-      fprintf(f,"%-10s%-10.4f%-24s%-20.8f%-20.8f\n", num2str(number), depth, dateStr, northVel, eastVel);
+for k1 = 1:length(H0) % цикл по глубинам, индекс - `k1`
+    depth = H0(k1); % берём `k1`-ю глубину
+    dateStr = datestr(t(k1)); % конвертируем utc формат даты в строку
+    aEastVel = Ves(k1,:);     % берём `e` скорости для `k1`-й глубины
+    aNorthVel = Vns(k1,:);    % берём `n` скорости для `k1`-й глубины
+    for k2 = 1:length(aEastVel)  % цикл по скоротям, индекс - `k2`
+      eastVel = aEastVel(k2); % берём `k2`-ю `e` скорость `k1`-й глубины
+      northVel = aNorthVel(k2); % берём `k2`-ю `n` скорость `k1`-й глубины
+      number = 1; % просто номер 1, чтобы было что вывести здесь. Можно вставить станции какие-нибудь, например.
+      fprintf(f,"%-10s%-10.4f%-24s%-20.8f%-20.8f\n", num2str(number), depth, dateStr, northVel, eastVel); % пишем строку в файл
     end
 end
-% Closing the file
-fclose(f);
+fclose(f); % закрываем файл
 end
 
 function convertFile(fname)
